f5e6c7d1c883525a112c8e5b4557730271367540
Valerii Viun <v2455521@ukr.net>
2024-01-23 17:26:52 +0200

    Update 8.ts

--- src/generic/8.ts
+++ src/generic/8.ts
@@ -25,3 +25,30 @@ type Params = Omit<Form, "errors">;
 // type Params = Pick<Form, Exclude<keyof Form, "errors">>;
 
 export {};
+
+class Animal {
+  public say() {
+    console.log("I'm an animal");
+  }
+}
+
+class Cat extends Animal {
+  public say() { // перевизначення методу say() для Cat
+    console.log('Meow');
+  }
+}
+
+class Dog extends Animal {
+  public say() { // перевизначення методу say() для Dog
+    console.log('Woof');
+  }
+}
+
+let animal = new Cat();
+animal.say(); // Виводить "Meow"
+
+animal = new Dog();
+animal.say(); // Виводить "Woof"
+
+
+
